12cd55851706f20b5e41fece964e25cf97a32171
Pooja Tirmare <pujatirmare@gmail.com>
2021-07-09 12:24:25 +0530

    update file

---
+++ src/TextInputGetValue.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export default function TextInput()
+{
+    const[data, setData] = useState(null)
+    const[print, setPrint] = useState(false)
+
+    function getData(val)
+    {
+        setData(val.target.value)
+        setPrint(false)
+    }
+    return(
+        <div>
+            {
+                print?
+             <h1>{data}</h1>
+             :null
+
+            }
+            
+            <input type = "text" onChange={getData}/>
+            <button onClick={()=>setPrint(true)}>PrintData</button>
+        </div>
+    )
+}
